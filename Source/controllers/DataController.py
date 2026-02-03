f9a74cf38d3c4a0b5605bab6fc7f34680e683e7c
Osama Elkhuriby <osama.elkhuriby@gmail.com>
2026-02-03 18:09:35 +0200

    Add ResponseSignal enum for file validation responses; update DataController and config settings

--- Source/controllers/DataController.py
+++ Source/controllers/DataController.py
@@ -1,13 +1,14 @@
 from .BaseController import BaseController
 from fastapi import UploadFile, File
+from models import ResponseSignal
 
 
 class DataController(BaseController):
     
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
 
     def validate_uploaded_file(self, file: UploadFile):
        if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
-           return False, "File type not allowed."
-       return True, "File is valid." 
+           return False, ResponseSignal.FILE_TYPE_NOT_SUPPORTED.value
+       return True, ResponseSignal.UPLOAD_SUCCESS.value 
