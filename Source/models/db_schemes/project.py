aa822dec8025095fe165edc6aac5ce983049819e
Osama Elkhuriby <osama.elkhuriby@gmail.com>
2026-02-10 07:00:45 +0200

    Add DataChunk and Project models with validation in db_schemes

---
+++ Source/models/db_schemes/project.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel , Field, validator
+from typing import Optional
+from bson.objectid import ObjectId as ObjectID
+
+
+class Project(BaseModel):
+    _id : Optional[ObjectID]
+    project_id : str = Field(...,min_length=1)
+
+
+    @validator('project_id')
+    def validate_project_id(cls, v):
+        if not v.isalnum():
+            raise ValueError('project_id must be alphanumeric')
+        return v
+
+    
+    class Config:
+        arbitrary_types_allowed = True
